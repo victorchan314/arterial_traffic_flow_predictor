2453bf95b9f80e121b80c0dfdad80739c1926b33
Victor Chan <victorchan88888888@gmail.com>
2018-12-11 16:28:04 -0800

    Added getters, constructor, and fitting functions to armax.py

--- armax.py
+++ armax.py
@@ -3,5 +3,39 @@ from statsmodels.tsa import arima_model
 
 class armax:
 
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, endog, exog=None):
+        self.endog = data
+        self.exog = exog
+        self._armax_models = {}
+        self.best_model = None
+        self.best_model_order = (-1, -1)
+        self.best_model_exog = False
+
+    def get_endog(self):
+        return self.endog
+
+    def get_exog(self):
+        return self.exog
+
+    def get_best_model(self):
+        return self.best_model
+
+    def get_best_model_order(self):
+        return self.best_model_order
+
+    def get_best_model_ar_order(self):
+        return self.best_model_order[0]
+
+    def get_best_model_ma_order(self):
+        return self.best_model_order[1]
+
+    def get_best_model_exog(self):
+        return self.best_model_exog
+
+    def fit(self, ar_max=5, ma_max=5):
+        self.grid_search(ar_max, ma_max)
+
+    def grid_search(self, ar_max=5, ma_max=5):
+        for ar in range(ar_max):
+            for ma in range(ma_max):
+                pass
