575028be94b7485a636a12737fe92c5066493f7e
Victor Chan <victorchan88888888@gmail.com>
2019-01-01 14:32:16 -0800

    Updated class docstring and fit models with grid search

--- armax.py
+++ armax.py
@@ -2,6 +2,23 @@ import numpy as np
 from statsmodels.tsa import arima_model
 
 class armax:
+    """
+    A wrapper class for statsmodels.tsa.arima_model.ARMA.
+
+    Attributes:
+        endog (np.array): Endogenous variable for ARMA model
+        exog (np.array): Optional exogenous variable for ARMA model
+        dates (np.array): Optional dates corresponding to endog and/or exog
+        preprocessed (bool): Whether the data has been preprocessed or not
+        preprocessing_method (str): Which preprocessing method has been used
+        preprocessed_endog (np.array): Preprocessed endog
+        preprocessed_exog (np.array): Preprocessed exog
+        w (int): Order of preprocessing
+        _armax_models (dict): Dictionary of all the armax models fit
+        best_model (arima_model.ARMAResults): Results of the best model fit
+        best_model_order (tuple): Model order with the lowest llf
+        best_model_exog (bool): 
+    """
 
     PREPROCESSING_SMOOTHING = "smoothing"
     PREPROCESSING_AGGREGATING = "aggregating"
@@ -11,6 +28,7 @@ class armax:
         self.endog = endog
         self.exog = exog
         self.dates = dates
+        self.preprocessed = False
         self.preprocessing_method = None
         self.preprocessed_endog = None
         self.preprocessed_exog = None
@@ -21,10 +39,16 @@ class armax:
         self.best_model_exog = False
 
     def get_endog(self):
-        return self.endog
+        if self.preprocessed:
+            return self.preprocessed_endog
+        else:
+            return self.endog
 
     def get_exog(self):
-        return self.exog
+        if self.preprocessed:
+            return self.preprocessed_endog
+        else:
+            return self.exog
     
     def get_dates(self):
         return self.dates
@@ -67,12 +91,14 @@ class armax:
     def preprocess(self, method, w):
         if method == self.PREPROCESSING_SMOOTHING:
             self.w = w
-            self.smoothed_endog = self._smooth(self.endog, w)
-            self.smoothed_exog = self._smooth(self.exog, w) if self.exog else None
+            self.preprocessed_endog = self._smooth(self.endog, w)
+            self.preprocessed_exog = self._smooth(self.exog, w) if self.exog else None
+            self.preprocessed = True
         elif method == self.PREPROCESSING_AGGREGATING:
             self.w = w
-            self.smoothed_endog = self._aggregate(self.endog, w)
-            self.smoothed_exog = self._aggregate(self.exog, w) if self.exog else None
+            self.preprocessed_endog = self._aggregate(self.endog, w)
+            self.preprocessed_exog = self._aggregate(self.exog, w) if self.exog else None
+            self.preprocessed = True
         else:
             raise ValueError("Illegal method passed: must be one of '{}'".format(self.PREPROCESSING_METHODS))
     
@@ -98,7 +124,15 @@ class armax:
         self.grid_search(ar_max, ma_max)
 
     def grid_search(self, ar_max=5, ma_max=5):
+        min_order = (0, 0)
+        min_likelihood = np.inf
+
         for ar in range(ar_max):
             for ma in range(ma_max):
                 order = (ar, ma)
-                self._armax_models[order] = arima_model.ARMA(self.endog, order, self.exog)
+                model = arima_model.ARMA(self.get_endog(), order, self.get_exog())
+
+                results = model.fit()
+                llf = results.llf
+
+                self._armax_models[order] = results
