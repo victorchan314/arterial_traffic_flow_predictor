4703e996812de4858a3db4e0a88882ee6928210a
Victor Chan <victorchan88888888@gmail.com>
2019-04-08 10:48:53 -0700

    Generated list of advanced sensors and removed model data from .gitignore

--- scripts/generate_graph_connections.py
+++ scripts/generate_graph_connections.py
@@ -4,9 +4,12 @@ import sys
 parent_dir = os.path.abspath("/Users/victorchan/Dropbox/UndergradResearch/Victor/Code")
 sys.path.append(parent_dir)
 
+#import numpy as np
+import pandas as pd
+
 import mysql_utils
 
-QUERY =\
+PHASE_TIMINGS_QUERY =\
 "SELECT IntersectionID, EndDate, EndTime, PhaseActualGreenTime FROM phase_time_2017 \
 WHERE (IntersectionID = 5083 OR IntersectionID = 6081 OR IntersectionID = 6082)\
     AND (EndDate > (\
@@ -25,10 +28,30 @@ WHERE (IntersectionID = 5083 OR IntersectionID = 6081 OR IntersectionID = 6082)\
     )\
 );"
 
+DETECTOR_INVENTORY_QUERY =\
+"SELECT IF(SensorID < 10, CONCAT(IntersectionID, 0, SensorID), CONCAT(IntersectionID, SensorID)) AS Sensor,\
+IntersectionID, SensorID, Direction, Movement FROM detector_inventory \
+WHERE Movement = 'Advanced' \
+AND (IntersectionID = 5083 OR IntersectionID = 6081 OR IntersectionID = 6082);"
+
+
+
+generate_sensors_advanced = False
+
 if __name__ == "__main__":
-    phase_timings = mysql_utils.execute_query(QUERY)
+    phase_timings = mysql_utils.execute_query(PHASE_TIMINGS_QUERY)
+    detector_inventory = mysql_utils.execute_query(DETECTOR_INVENTORY_QUERY)
     
-    if phase_timings == None:
+    if phase_timings == None or detector_inventory == None:
         sys.exit()
 
-    print(len(phase_timings))
+    phase_timings = pd.DataFrame(phase_timings, columns=["IntersectionID", "EndDate", "EndTime", "PhaseTimings"])
+    detector_inventory = pd.DataFrame(detector_inventory, columns=["Sensor", "IntersectionID", "SensorID", "Direction", "Movement"])
+    edges = pd.read_csv("data/model/edges.csv", header=None)
+    phases = pd.read_csv("data/model/phases.csv")
+
+    if generate_sensors_advanced:
+        detector_inventory["Sensor"].to_csv("data/model/sensors_advanced.csv", index=False)
+    
+    #print(phase_timings.head())
+    print(detector_inventory)
