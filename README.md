56922e8ca0578ab63bfe878a9f1e15332351ac95
Victor Chan <victorchan88888888@gmail.com>
2019-09-09 17:56:38 -0700

    Reorganize repository

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 # Arterial Traffic Flow Prediction
 
+This repository contains all of the code that I have been using for my research.
+
 ### Random notes about DCRNN
 
 - Data loading is weird. At first glance, it seems to only run through the data once, replicating the last value if necessary, and if it runs out of data, does not supply any more data. This might be avoided if multiple DataLoaders are created.
 - Might want to bootstrap (at least the replications)
 - StandardScaler only scales the first dimension of the input (maybe to leave time unaffected). Might want to change it so that time is the first dimension and everything else is scaled, or time is the second dimension and everything else is scaled.
+- I think the way the word "epoch" is used is incorrect; not a huge deal, but just misleading
+- dcrnn_cell:103: Could be (1 - u) * state + u * c; in theory is the same, though
+- dcrnn_cell:165: Is there an extra 2 times the last extraneous term in the sum?
+- dcrnn_model:49: It looks like the decoding cell is the same cell as the encoding cell, which may not perform as well, according to Sutskever's paper
+- dcrnn_model:39: Why are the labels using input_dim instead of output_dim?
+
+### TODO
+
+- Baseline methods visualization and table
+- Predictions on dummy data that is linear
+- Try predictions for only 1 or 2 detectors with full coverage
+- Flow and occupancy and flow / occupancy
+- Visualization library
---
+++ old/main.py
@@ -0,0 +1,70 @@
+import sys
+
+import numpy as np
+import datetime as dt
+
+from armax import armax
+from config import config
+import mysql_utils as mysql
+import visualization
+
+
+#DETECTOR_DATA_TABLE = "detector_data_processed_2017_1"
+DETECTOR_DATA_TABLE = "detector_data_processed_2017"
+DETECTOR_ID = "608219"
+DETECTOR_DATA_QUERY = "SELECT DetectorID, Year, Month, Day, Time, Volume, Occupancy \
+        FROM {} AS DD NATURAL JOIN detector_health AS DH \
+        WHERE DetectorID = {} AND Health = 1 \
+        ORDER BY Year, Month, Day, Time;"
+
+
+def query_detector_data(cursor, table, detector_id, graph=False):
+    query = DETECTOR_DATA_QUERY.format(table, detector_id)
+
+    cursor = mysql.query(cursor, query)
+    
+    if cursor == None:
+        return
+
+    time = []
+    volume = []
+    occupancy = []
+    #speed = []
+
+    for row in cursor:
+        d = dt.datetime(row[1], row[2], row[3], row[4] // 3600, (row[4] % 3600) // 60, row[4] % 60)
+        time.append(d)
+
+        volume.append(row[5])
+        occupancy.append(row[6])
+        #speed.append(row[7])
+
+    time = np.array(time)
+    volume = np.array(volume)
+    occupancy = np.array(occupancy)
+    occupancy_percentage = occupancy / 3600 * 100
+    #speed = np.array(speed)
+
+    if graph:
+        visualization.plot_data_over_time(time, volume, title="Detector {} Volume 2017".format(detector_id), ylabel="Volume (vph)", figsize=(12, 5))
+        visualization.plot_data_over_time(time, occupancy, title="Detector {} Occupancy 2017".format(detector_id), ylabel="Occupancy (s)", figsize=(12, 5))
+        #visualization.plot_data_over_time(time, speed, title="Detector {} Speed 2017".format(detector_id), ylabel="Speed", figsize=(12, 5))
+        visualization.plot_data_over_time(time, occupancy_percentage, title="Detector {} Occupancy 2017".format(detector_id), ylabel="Occupancy (%)", figsize=(12, 5))
+        visualization.plot_fundamental_diagram(volume, occupancy_percentage, title="Detector {} Flow-Occupancy Diagram 2017".format(detector_id))
+
+    return time, volume, occupancy
+
+
+
+if __name__ == '__main__':
+    cnx = mysql.connect_to_database(**config)
+
+    if cnx == None:
+        sys.exit()
+
+    cursor = cnx.cursor()
+
+    time, flow, occupancy = query_detector_data(cursor, DETECTOR_DATA_TABLE, DETECTOR_ID)
+
+    cursor.close()
+    cnx.close()
