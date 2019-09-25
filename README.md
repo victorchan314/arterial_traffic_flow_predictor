40b7c36728054ad45935e901468860ab7ea8dd50
Victor Chan <victorchan88888888@gmail.com>
2019-09-25 13:02:24 -0700

    Get train and val errors from log for DCRNN

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 This repository contains all of the code that I have been using for my research.
 
+All commands must be run from the top-level Code directory.
+
+Command to generate distances and adjacency matrix
+
+    python3 scripts/generate_graph_connections.py --intersection 5083 --plan_name P1 --adjacency_matrix_path data/inputs/model/distances_5083_P1.csv
+    python3 DCRNN/scripts/gen_adj_mx.py --sensor_ids_filename data/inputs/model/sensors_advanced_5083.txt --distances data/inputs/model/distances_5083_P1.csv --output_pkl_filename data/inputs/model/adjacency_matrix_5083_P1.pkl
+
 Command to run the training data generation script
 
     python3 scripts/generate_training_data.py --intersection 5083 --plan_name P2 --output_dir data/inputs/5083_sensor_data -v
@@ -22,21 +29,29 @@ Command to plot predictions
 
     python3 DCRNN/scripts/graph_predictions.py data/5083/predictions.npz data/inputs/5083_sensor_data/test.npz
 
-### Random notes about DCRNN
+### Notes about DCRNN
+
+- I think the way the word "epoch" is used is incorrect; not a huge deal, but just misleading
+- dcrnn_cell:103: Could be (1 - u) * state + u * c; in theory is the same, though
+- Validation loss is used for early stopping regularization
+- `self._test_model` uses the variables from `self._train_model` because they share the same variable scope
+
+### Confusions/Weird things
 
 - Data loading is weird. At first glance, it seems to only run through the data once, replicating the last value if necessary, and if it runs out of data, does not supply any more data. This might be avoided if multiple DataLoaders are created.
 - Might want to bootstrap (at least the replications)
 - StandardScaler only scales the first dimension of the input (maybe to leave time unaffected). Might want to change it so that time is the first dimension and everything else is scaled, or time is the second dimension and everything else is scaled.
-- I think the way the word "epoch" is used is incorrect; not a huge deal, but just misleading
-- dcrnn_cell:103: Could be (1 - u) * state + u * c; in theory is the same, though
+- Change DCRNN so that it predicts with the shape (num_data, offsets, num_detectors, num_dimensions) and that time is not included in the output
 - dcrnn_cell:165: Is there an extra 2 times the last extraneous term in the sum?
 - dcrnn_model:49: It looks like the decoding cell is the same cell as the encoding cell, which may not perform as well, according to Sutskever's paper
 - dcrnn_model:39: Why are the labels using input_dim instead of output_dim?
-- Validation loss is used for early stopping regularization
-- Change DCRNN so that it predicts with the shape (num_data, offsets, num_detectors, num_dimensions) and that time is not included in the output
+- dcrnn_model:48-49: Uses the same cell * `num_layers`
 
 ### TODO
 
+- Plan P1
+- Add different kinds of errors to output
+- Step through data to debug constant shifting
 - Baseline methods visualization and table
 - Predictions on dummy data that is linear
 - Try predictions for only 1 or 2 detectors with full coverage
--- models/dcrnn_wrapper.py
+++ models/dcrnn_wrapper.py
@@ -1,9 +1,7 @@
 import os
 import sys
+import re
 import yaml
-
-parent_dir = os.path.abspath("/Users/victorchan/Desktop/UC Berkeley/Research/Code")
-sys.path.append(parent_dir)
 
 import numpy as np
 import tensorflow as tf
@@ -17,14 +15,17 @@ from DCRNN.model.dcrnn_supervisor import DCRNNSupervisor
 
 class DCRNN(Model):
     """Wrapper that runs DCRNN and saves the results"""
-    def __init__(self, *args, config_filename=None, output_filename=None, predictions_filename=None,
+    def __init__(self, *args, config_filename=None, is_trained=False, output_filename=None, predictions_filename=None,
                  use_cpu_only=False, **kwargs):
+        super(DCRNN, self).__init__(*args, **kwargs)
+
         if not config_filename:
             raise ValueError("config_filename is a required argument")
 
         self.config_filename = config_filename
         self.output_filename = output_filename
         self.predictions_filename = predictions_filename
+        self._is_trained = is_trained
 
         with open(config_filename) as f:
             self.supervisor_config = yaml.load(f)
@@ -38,22 +39,54 @@ class DCRNN(Model):
         self.tf_config.gpu_options.allow_growth = True
         self.supervisor = DCRNNSupervisor(adj_mx=self.adj_mx, **self.supervisor_config)
 
-        super(DCRNN, self).__init__(*args, **kwargs)
+    def train(self):
+        self._train()
 
     def _train(self):
-        if self.output_filename:
-            tee = Tee()
+        if not self._is_trained:
+            self._is_trained = True
+            if self.output_filename:
+                tee = Tee(self.output_filename)
 
-        with tf.Session(config=self.tf_config) as sess:
-            self.supervisor.train(sess=sess)
+            with tf.Session(config=self.tf_config) as sess:
+                self.supervisor.train(sess=sess)
 
-        if self.output_filename:
-            del tee
+            if self.output_filename:
+                del tee
+        else:
+            with tf.Session(config=self.tf_config) as sess:
+                if "train" in self.supervisor_config and "model_filename" in self.supervisor_config["train"]:
+                    self.supervisor.load(sess, self.supervisor_config['train']['model_filename'])
+
+        self.get_errors_from_log_file()
 
     def predict(self, x):
         with tf.Session(config=self.tf_config) as sess:
-            supervisor = DCRNNSupervisor(adj_mx=self.adj_mx, **self.supervisor_config)
-            supervisor.load(sess, self.supervisor_config['train']['model_filename'])
-            outputs = supervisor.evaluate(sess)
-            np.savez_compressed(self.output_filename, **outputs)
-            print('Predictions saved as {}.'.format(self.output_filename))
+            outputs = self.supervisor.evaluate(sess)
+            np.savez_compressed(self.predictions_filename, **outputs)
+            print('Predictions saved as {}.'.format(self.predictions_filename))
+
+    def get_errors_from_log_file(self):
+        if "train" in self.supervisor_config and "log_dir" in self.supervisor_config["train"]:
+            path = "{}{}".format(self.supervisor_config["train"]["log_dir"], "info.log")
+        else:
+            path = self.supervisor._log_dir
+
+        last_errors_message = None
+        with open(path) as f:
+            for line in f:
+                message = line.split(" - ")[-1]
+                if message.startswith("Epoch"):
+                    last_errors_message = message
+                elif line.startswith("Horizon"):
+                    pass
+
+        last_errors = re.findall("[A-Za-z_]+: ?[-+]?\d*\.\d+", last_errors_message)
+        errors = {}
+
+        for error in last_errors:
+            key, value = re.compile(": ?").split(error)
+            errors[key] = float(value)
+
+        self.errors["train"] = {"mae": errors["train_mae"]}
+        self.errors["val"] = {"mae": errors["val_mae"]}
