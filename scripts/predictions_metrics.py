a3fe28116d872b01f19970d92a97ef2f7cfa74d1
Victor Chan <victorchan88888888@gmail.com>
2020-02-10 03:52:00 -0800

    Calculate errors per feature

--- scripts/predictions_metrics.py
+++ scripts/predictions_metrics.py
@@ -16,6 +16,18 @@ from lib import utils
 
 PREDICTIONS_FILENAME = "predictions.npz"
 
+
+
+def load_predictions(predictions_path, detectors=None, detector_list=None):
+    groundtruth, predictions = utils.load_predictions(predictions_path)
+
+    if not detectors is None:
+        detectors_mask = [True if detector in detectors else False for detector in detector_list]
+        groundtruth = groundtruth[:, :, detectors_mask, ...]
+        predictions = predictions[:, :, detectors_mask, ...]
+
+    return groundtruth, predictions
+
 def print_errors(logdir, horizons, precision):
     dirs = sorted(os.listdir(logdir))
 
@@ -39,7 +51,7 @@ def print_errors(logdir, horizons, precision):
 
             print()
 
-def print_errors_latex(logdir, horizons, precision, detectors=None, detector_list=None, ts_dir=None, phase_plans=None):
+def print_errors_latex(logdir, horizons, features, precision, detectors=None, detector_list=None, ts_dir=None, phase_plans=None):
     dirs = sorted(os.listdir(logdir))
 
     error_types = ["mse", "rmse", "mae", "mape"]
@@ -56,15 +68,12 @@ def print_errors_latex(logdir, horizons, precision, detectors=None, detector_lis
 
         offset = int(offset_matches[0][2:-1])
 
-        groundtruth, predictions = utils.load_predictions(predictions_path)
-
-        if not detectors is None:
-            detectors_mask = [True if detector in detectors else False for detector in detector_list]
-            groundtruth = groundtruth[:, :, detectors_mask, ...]
-            predictions = predictions[:, :, detectors_mask, ...]
+        groundtruth, predictions = load_predictions(predictions_path, detectors=detectors, detector_list=detector_list)
 
         horizon = predictions.shape[0]
         horizons = range(1, horizon + 1) if horizons is None else sorted([int(h) for h in horizons])
+        num_features = 1 if len(predictions.shape) < 4 else predictions.shape[3]
+        features = range(num_features) if features is None else sorted([int(f) - 1 for f in features])
         batch_size = predictions.shape[1]
 
         if not ts_dir is None:
@@ -90,66 +99,72 @@ def print_errors_latex(logdir, horizons, precision, detectors=None, detector_lis
 
         for plan in plans:
             if not plan in errors:
-                errors[plan] = {e: {} for e in error_types}
+                errors[plan] = {f: {} for f in features}
 
-            for v in errors[plan].values():
-                if not offset in v:
-                    v[offset] = {}
+            for feature in features:
+                errors[plan][feature] = {e: {} for e in error_types}
+
+                for v in errors[plan][feature].values():
+                    if not offset in v:
+                        v[offset] = {}
 
         for plan in plans:
             groundtruth = all_groundtruths[plan]
             predictions = all_predictions[plan]
 
-            for h in horizons:
-                prediction_errors = data_utils.get_standard_errors(groundtruth[h-1, ...], predictions[h-1, ...])
+            for f in features:
+                for h in horizons:
+                    prediction_errors = data_utils.get_standard_errors(groundtruth[h-1, :, :, f], predictions[h-1, :, :, f])
 
-                for k, v in prediction_errors.items():
-                    error = v
-                    if h in errors[plan][k][offset]:
-                        previous_error = errors[plan][k][offset][h][0]
-                        previous_batch_size = errors[plan][k][offset][h][1]
-                        total_batch_size = batch_size + previous_batch_size
-                        average_error = (error * batch_size + previous_error * previous_batch_size) / total_batch_size
+                    for k, v in prediction_errors.items():
+                        error = v
+                        if h in errors[plan][f][k][offset]:
+                            previous_error = errors[plan][f][k][offset][h][0]
+                            previous_batch_size = errors[plan][f][k][offset][h][1]
+                            total_batch_size = batch_size + previous_batch_size
+                            average_error = (error * batch_size + previous_error * previous_batch_size) / total_batch_size
 
-                        errors[plan][k][offset][h] = [average_error, total_batch_size]
-                    else:
-                        errors[plan][k][offset][h] = [error, batch_size]
+                            errors[plan][f][k][offset][h] = [average_error, total_batch_size]
+                        else:
+                            errors[plan][f][k][offset][h] = [error, batch_size]
 
     for plan, v1 in errors.items():
-        for error_type, v2 in v1.items():
-            for offset, v3 in v2.items():
-                for horizon, v4 in v3.items():
-                    v = v4[0]
-                    if v >= 1000:
-                        error = int(v)
-                    elif error_type == "mape":
-                        error = round(100 * v, precision)
-                    else:
-                        error = round(v, precision)
+        for feature, v2 in v1.items():
+            for error_type, v3 in v2.items():
+                for offset, v4 in v3.items():
+                    for horizon, v5 in v4.items():
+                        v = v5[0]
+                        if v >= 1000:
+                            error = int(v)
+                        elif error_type == "mape":
+                            error = round(100 * v, precision)
+                        else:
+                            error = round(v, precision)
 
-                    errors[plan][error_type][offset][horizon] = error
+                        errors[plan][feature][error_type][offset][horizon] = error
 
     output = ""
     for plan in sorted(errors.keys()):
-        output = utils.proxy_print(output, plan)
+        for feature in sorted(errors[plan].keys()):
+            output = utils.proxy_print(output, "Plan {}, feature {}".format(plan, feature))
 
-        for error_type in error_types:
-            table_line_errors = errors[plan][error_type]
-            table_line = "& " + error_type.upper()
-            for offset in sorted(table_line_errors.keys()):
-                for horizon in sorted(table_line_errors[offset].keys()):
-                    error = table_line_errors[offset][horizon]
-                    if len(str(error)) > precision + 5:
-                        error = ("{:." + str(precision) + "E}").format(error)
+            for error_type in error_types:
+                table_line_errors = errors[plan][feature][error_type]
+                table_line = "& " + error_type.upper()
+                for offset in sorted(table_line_errors.keys()):
+                    for horizon in sorted(table_line_errors[offset].keys()):
+                        error = table_line_errors[offset][horizon]
+                        if len(str(error)) > precision + 5:
+                            error = ("{:." + str(precision) + "E}").format(error)
 
-                    if error_type == "mape":
-                        table_line += " & {}\\%".format(error)
-                    else:
-                        table_line += " & {}".format(error)
+                        if error_type == "mape":
+                            table_line += " & {}\\%".format(error)
+                        else:
+                            table_line += " & {}".format(error)
 
-            table_line += " \\\\"
+                table_line += " \\\\"
 
-            output = utils.proxy_print(output, table_line)
+                output = utils.proxy_print(output, table_line)
 
     return output
 
@@ -160,6 +175,7 @@ def main(args):
     detectors = args.detectors
     detector_list_path = args.detector_list_path
     horizons = args.horizons
+    features = args.features
     not_latex = args.not_latex
     precision = args.round
     ts_dir = args.no_plan_ts_dir
@@ -186,15 +202,16 @@ def main(args):
         else:
             print(output)
     else:
-        return print_errors_latex(logdir, horizons, precision, detectors=detectors, detector_list=detector_list,
-                           ts_dir=ts_dir, phase_plans=phase_plans)
+        return print_errors_latex(logdir, horizons, features, precision, detectors=detectors,
+                                  detector_list=detector_list, ts_dir=ts_dir, phase_plans=phase_plans)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("logdir", type=str, help="Location of experiment directories")
     parser.add_argument("--detectors", "-d", type=int, nargs="+", help="Detectors to calculate error for")
     parser.add_argument("--detector_list_path", "--dl", type=str, help="Order of detectors in predictions")
-    parser.add_argument("--horizons", "--h", action="append", help="Horizon(s) to include")
+    parser.add_argument("--horizons", "--h", nargs="+", help="Horizon(s) to include")
+    parser.add_argument("--features", "--f", nargs="+", help="Feature(s) to include")
     parser.add_argument("--not_latex", "-l", action="store_true", help="Don't print for LaTeX table")
     parser.add_argument("--round", "-r", type=int, default=2, help="Rounding precision")
     parser.add_argument("--do_not_print", action="store_true", help="Flag to suppress printing")
@@ -202,4 +219,5 @@ if __name__ == "__main__":
     parser.add_argument("--phase_plans_csv", type=str, help="Location of csv with phase plan times")
     args = parser.parse_args()
 
-    main(args)
+    output = main(args)
+    print(output)
