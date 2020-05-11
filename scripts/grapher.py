5d74a675503c65e1afd333332471f9ea322eb223
Victor Chan <victorchan88888888@gmail.com>
2020-05-11 03:20:28 -0700

    Graph predictions of DCRNN by horizon and by time

--- scripts/grapher.py
+++ scripts/grapher.py
@@ -6,12 +6,15 @@ parent_dir = os.path.abspath(".")
 sys.path.append(parent_dir)
 
 import numpy as np
+import datetime as dt
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from matplotlib.lines import Line2D
 import seaborn as sns
 import pandas as pd
 
+from lib import data_utils
+from lib import utils
 from scripts.generate_training_data import get_detector_data
 
 
@@ -19,6 +22,8 @@ sns_colors = sns.color_palette("colorblind")
 cycle = [sns_colors[i] for i in [2, 9, 6, 4, 8, 0, 3, 1, 7, 5]]
 mpl.rcParams["axes.prop_cycle"] = mpl.cycler(color=cycle)
 colors = cycle
+
+DETECTOR_DATA_FREQUENCY = dt.timedelta(minutes=5)
 
 
 def get_one_detector_data(detector):
@@ -110,6 +115,97 @@ def graph_detector_data_time_series(detector_id, xticks_datetime_precision="D", 
 
     plt.show()
 
+def extract_flat_data(timestamps_array, groundtruth_array):
+    stretches = data_utils.get_stretches(timestamps_array[:, 0], DETECTOR_DATA_FREQUENCY)
+    timestamps = data_utils.flatten_circulant_like_matrix_by_stretches(timestamps_array, stretches)
+    groundtruth_array_transposed = np.swapaxes(groundtruth_array, 0, 1)
+    groundtruth = data_utils.flatten_circulant_like_matrix_by_stretches(groundtruth_array_transposed, stretches)
+
+    return timestamps, groundtruth
+
+def fit_dates_to_timestamps(full_x, original_x, new_x):
+    return new_x[np.isin(full_x, original_x)]
+
+def graph_predictions(y, y_hat, x, x_array, sensor, step=4, title=None, num_xticks=12, xticks_datetime_precision="D"):
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(16, 6))
+    fig.suptitle(title)
+
+    xticks = np.arange(x.shape[0])
+    xticks_labels = np.datetime_as_string(x, unit=xticks_datetime_precision)
+    xticks_locs = [xticks[x.shape[0] // num_xticks * i] for i in range(num_xticks)] + [xticks[-1]]
+    xticks_spaced_labels = [xticks_labels[x.shape[0] // num_xticks * i] for i in range(num_xticks)] + [xticks_labels[-1]]
+
+    for ax in (ax1, ax2):
+        ax.set(xlabel="Time", ylabel="Flow (vph)")
+        ax.set_xticks(xticks_locs)
+        ax.set_xticklabels(xticks_spaced_labels)
+
+        ax.plot(xticks, y[:, sensor], label="Ground Truth")
+
+    cmap = plt.get_cmap("jet")
+    reordered_colors =[6, 5, 2]
+
+    for i, h in enumerate([1, 3, 6]):
+        stretches = data_utils.get_stretches(x_array[:, h - 1], DETECTOR_DATA_FREQUENCY)
+        color = colors[reordered_colors[i]]
+
+        for start, end in stretches:
+            x_stretch = x_array[start:end, h - 1]
+            y_hat_stretch = y_hat[h - 1, start:end, sensor]
+            x_stretch_range = fit_dates_to_timestamps(x, x_stretch, xticks)
+
+            if start == 0:
+                label = "{} min predictions".format(h * 5)
+            else:
+                label = None
+
+            ax1.plot(x_stretch_range, y_hat_stretch, label=label, c=color, alpha=0.6)
+
+    ax1.legend()
+    ax1.set_xlim(xticks.shape[0] * 4 / 7 - 13, xticks.shape[0])
+
+    ax2.legend()
+    stretches = data_utils.get_stretches(x_array[:, 0], DETECTOR_DATA_FREQUENCY)
+    for i, (start, end) in enumerate(stretches):
+        for t in range(start, end, step):
+            color = cmap((t - start) / (end - start - 1))
+            x_stretch = x_array[t, :]
+            y_hat_stretch = y_hat[:, t, sensor]
+            x_stretch_range = fit_dates_to_timestamps(x, x_stretch, xticks)
+
+            if start == 0:
+                label = "Time {} predictions".format(t)
+            else:
+                label = None
+
+            ax2.plot(x_stretch_range, y_hat_stretch, label=label, c=color, alpha=0.6)
+
+    ax2.set_xlim(xticks.shape[0] * 4 / 8, xticks.shape[0] * 6 / 8)
+
+    plt.show()
+
+def graph4():
+    logdirs = [x for x in os.listdir("experiments") if x.startswith("full-information_")]
+    if len(logdirs) > 1:
+        raise ValueError("More than 1 full-information logdir: {}".format(logdirs))
+
+    predictions_path = os.path.join("experiments", logdirs[0], "experiments", "dcrnn", "P2_o12_h6_sb12", "predictions.npz")
+    timestamps_path = os.path.join("experiments", logdirs[0], "inputs", "sensor_data", "P2_o12_h6_sb12_sensor_data", "test.npz")
+
+    groundtruth, predictions = utils.load_predictions(predictions_path)
+    groundtruth = groundtruth[..., 0]
+    predictions = predictions[..., 0]
+
+    timestamps_array = np.load(timestamps_path)["timestamps_y"]
+    timestamps, groundtruth = extract_flat_data(timestamps_array, groundtruth)
+
+    sensors = {508302: 0, 508306: 1}
+    detector = 508306
+    sensor = sensors[detector]
+
+    graph_predictions(groundtruth, predictions, timestamps, timestamps_array, sensor=sensor,
+                      title="Detector {} Full Information Test Predictions".format(detector))
+
 
 def main(args):
     graph = args.graph
@@ -121,6 +217,8 @@ def main(args):
         plot_fundamental_diagram_color_by_plan(508306)
     elif graph == 3:
         graph_detector_data_time_series(608107)
+    elif graph == 4:
+        graph4()
     else:
         raise ValueError("Invalid graph ID")
 
